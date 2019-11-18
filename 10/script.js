81588292c04d9243ecd7bd509c1ef32afbba83c6
Helga Fo-ox <roganova.0lga@yandex.ru>
2019-11-18 15:23:50 +0300

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -13,65 +13,64 @@ function mamSad() {
 }
 
 //MAIN
-var isMomHappy = false;
-var momAnswer = document.getElementById("answer");
-var option = document.getElementById("selected");
-option.addEventListener("click", function() {
-    if(option.value != "none" & option.value !="love")
-    {
-       let procent = randomStr() + +option.value;
-       console.log(procent);
-       momAnswer.innerHTML = "Мама думает...";
-       if (procent >= 100) {
-         isMomHappy = true;
-       }
-       else {
-       isMomHappy = false;
-       }
-       // Promise
-        var willIGetNewPhone = new Promise(
-            function (resolve, reject) {
-                if (isMomHappy) {
-                    var good = mamHappy();
-                    resolve(good); // Всё выполнено
-                } else {
-                    var str = mamSad();
-                    throw `${str}`;
-                    var bad = new Error(str);
-                    reject(bad); // reject
-                }
-            }
-        );
-              // Вызываем промис
-        var askMom = function () {
-            willIGetNewPhone
-                .then(function (fulfilled) {
-                    // yay, you got a new phone
-                    momAnswer.innerHTML = fulfilled;
-                    //console.log(fulfilled);
-                 // output: { brand: 'Samsung', color: 'black' }
-                })
-                .catch(function (error) {
-                    // oops, mom don't buy it
-                    momAnswer.innerHTML = error;
-                    //console.log(error);
-                 // output: 'mom is not happy'
-                });
-        };
-       setTimeout(()=>{
-          askMom();
-       }, 3000);
-    } if (option.value == "none") {
-    momAnswer.innerHTML = "";
-    }
-    if (option.value == "love") {
-    momAnswer.innerHTML = "Мама тебя любит :3";
-    }
-});
-
+function ready () {
+  var isMomHappy = false;
+  var momAnswer = document.getElementById("answer");
+  var option = document.getElementById("selected");
+  option.addEventListener("click", function() {
+      if(option.value != "none" & option.value !="love")
+      {
+         let procent = randomStr() + +option.value;
+         console.log(procent);
+         momAnswer.innerHTML = "Мама думает...";
+         if (procent >= 100) {
+           isMomHappy = true;
+         }
+         else {
+         isMomHappy = false;
+         }
+         // Promise
+          var willIGetNewPhone = new Promise(
+              function (resolve, reject) {
+                  if (isMomHappy) {
+                      var good = mamHappy();
+                      resolve(good); // Всё выполнено
+                  } else {
+                      var str = mamSad();
+                      throw `${str}`;
+                      var bad = new Error(str);
+                      reject(bad); // reject
+                  }
+              }
+          );
+                // Вызываем промис
+          var askMom = function () {
+              willIGetNewPhone
+                  .then(function (fulfilled) {
+                      // yay, you got a new phone
+                      momAnswer.innerHTML = fulfilled;
+                      //console.log(fulfilled);
+                   // output: { brand: 'Samsung', color: 'black' }
+                  })
+                  .catch(function (error) {
+                      // oops, mom don't buy it
+                      momAnswer.innerHTML = error;
+                      //console.log(error);
+                   // output: 'mom is not happy'
+                  });
+          };
+         setTimeout(()=>{
+            askMom();
+         }, 3000);
+      } if (option.value == "none") {
+      momAnswer.innerHTML = "";
+      }
+      if (option.value == "love") {
+      momAnswer.innerHTML = "Мама тебя любит :3";
+      }
+  });
+}
 //askMom();
-
-
 
 
 document.addEventListener("DOMContentLoaded", ready);
