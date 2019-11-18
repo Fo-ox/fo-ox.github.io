ec4ab917bc78b8edea29d018cfc37dc7753a61e9
Helga Fo-ox <roganova.0lga@yandex.ru>
2019-11-18 15:16:17 +0300

    10 дз обновление

--- 10/script.js
+++ 10/script.js
@@ -1,34 +1,3 @@
-var isMomHappy = false;
-// Promise
-var willIGetNewPhone = new Promise(
-    function (resolve, reject) {
-        if (isMomHappy) {
-            var good = setTimeout(mamHappy, 2000);
-            resolve(good); // Всё выполнено
-        } else {
-            var str = setTimeout(mamSad, 2000);
-            var bad = new Error(str);
-            reject(bad); // reject
-        }
-
-    }
-);
-
-// Вызываем промис
-var askMom = function () {
-    willIGetNewPhone
-        .then(function (fulfilled) {
-            // yay, you got a new phone
-            return(fulfilled.message);
-         // output: { brand: 'Samsung', color: 'black' }
-        })
-        .catch(function (error) {
-            // oops, mom don't buy it
-            return(error.message);
-         // output: 'mom is not happy'
-        });
-};
-
 
 // фунция возвращающая рандомное число в заданном диапазоне
 function randomStr (){
@@ -36,34 +5,74 @@ function randomStr (){
 }
 
 function mamHappy() {
-  return "Мама счастлива и купит вам то что вы хотите";
+  return "Мама счастлива и дала денег на покупку";
 }
 
 function mamSad() {
-  return "Мама не в настроении, подойдите позже";
+  return "Мама не в настроении, попробуй позже";
 }
 
 //MAIN
+var isMomHappy = false;
+var momAnswer = document.getElementById("answer");
+var option = document.getElementById("selected");
+option.addEventListener("click", function() {
+    if(option.value != "none" & option.value !="love")
+    {
+       let procent = randomStr() + +option.value;
+       console.log(procent);
+       momAnswer.innerHTML = "Мама думает...";
+       if (procent >= 100) {
+         isMomHappy = true;
+       }
+       else {
+       isMomHappy = false;
+       }
+       // Promise
+        var willIGetNewPhone = new Promise(
+            function (resolve, reject) {
+                if (isMomHappy) {
+                    var good = mamHappy();
+                    resolve(good); // Всё выполнено
+                } else {
+                    var str = mamSad();
+                    throw `${str}`;
+                    var bad = new Error(str);
+                    reject(bad); // reject
+                }
+            }
+        );
+              // Вызываем промис
+        var askMom = function () {
+            willIGetNewPhone
+                .then(function (fulfilled) {
+                    // yay, you got a new phone
+                    momAnswer.innerHTML = fulfilled;
+                    //console.log(fulfilled);
+                 // output: { brand: 'Samsung', color: 'black' }
+                })
+                .catch(function (error) {
+                    // oops, mom don't buy it
+                    momAnswer.innerHTML = error;
+                    //console.log(error);
+                 // output: 'mom is not happy'
+                });
+        };
+       setTimeout(()=>{
+          askMom();
+       }, 3000);
+    } if (option.value == "none") {
+    momAnswer.innerHTML = "";
+    }
+    if (option.value == "love") {
+    momAnswer.innerHTML = "Мама тебя любит :3";
+    }
+});
 
-function ready () {
-  var isMomHappy = true;
-  var momAnswer = document.getElementById("answer");
-  var option = document.getElementById("selected");
-  option.addEventListener("click", function() {
-      if(option.value != "none")
-      {
-         let procent = randomStr() + +option.value;
-         console.log(procent);
-         momAnswer.innerHTML = "Мама думает...";
-         let mamAnswer;
-         if (procent >= 100) {
-           isMomHappy = true;
-         }
-         mamAnswer = askMom();
-         console.log(mamAnswer);
-      }
-  });
-}
+//askMom();
+
+
+
 
 document.addEventListener("DOMContentLoaded", ready);
 
